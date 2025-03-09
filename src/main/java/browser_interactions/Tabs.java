dc6743c040462e09a7cb0c31484c1f890f5f849a
swynlen <84979539+swynlen@users.noreply.github.com>
2025-03-09 13:02:19 +0000

    0603 done

--- src/main/java/browser_interactions/Tabs.java
+++ src/main/java/browser_interactions/Tabs.java
@@ -12,7 +12,16 @@ public class Tabs extends Main {
     driver.findElement(By.id("a-link-that-opens-a-new-window")).click();
     var windows = driver.getWindowHandles();
     System.out.println(windows);
-    
+
+    String originalWindow = driver.getWindowHandle();
+    for (String window : windows) {
+      if (!window.equals(originalWindow)) {
+        driver.switchTo().window(window);
+        break;
+      }
+    }
+
+    System.out.println(driver.getTitle());
 
     driver.quit();
   }
